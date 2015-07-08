38071078cd032f1a1ed49420065ea29474169a82
Ivan Li <ivankyli@gmail.com>
2015-07-08 14:36:11 +1200

    Fixing missing resource type for new version specifier

--- resources/default.rb
+++ resources/default.rb
@@ -2,7 +2,7 @@ actions :install, :remove, :upgrade
 
 attribute :package, :kind_of => String, :name_attribute => true
 attribute :source, :kind_of => String
-attribute :version, :kind_of => String
+attribute :version, :kind_of => [String, Symbol]
 attribute :args, :kind_of => String
 attribute :options, :kind_of => Hash, :default => {}
 
